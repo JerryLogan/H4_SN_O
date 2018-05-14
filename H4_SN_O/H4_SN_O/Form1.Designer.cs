ed56db0b78de64f3fdc072957a034ca7a706743e
jerrylo <s86186654418@hotmail.com>
2018-05-14 16:12:37 +0800

    add whole project

---
+++ H4_SN_O/H4_SN_O/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace H4_SN_O
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox_SN_1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "select template";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 137);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(222, 250);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // textBox_SN_1
+            // 
+            this.textBox_SN_1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox_SN_1.Location = new System.Drawing.Point(137, 12);
+            this.textBox_SN_1.Name = "textBox_SN_1";
+            this.textBox_SN_1.Size = new System.Drawing.Size(191, 36);
+            this.textBox_SN_1.TabIndex = 2;
+            this.textBox_SN_1.Text = "1234567890123456";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(12, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(316, 77);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(481, 139);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(222, 248);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(709, 139);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(222, 248);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 153);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_SN_1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox_SN_1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ H4_SN_O/H4_SN_O/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace H4_SN_O
+{
+    public partial class Form1 : Form
+    {
+        String lineline;
+        String[] template_content_262 = new string[262];
+        String[] SN_split_16 = new String[16];
+        String SN_str;
+        String desktop_path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        String folder_path;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //select template
+        {
+            int line_count =0;
+            folder_path = desktop_path + @"\H4_SN_0";
+            DirectoryInfo di = Directory.CreateDirectory(folder_path);
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr_txt = new StreamReader(openFileDialog1.FileName);
+                //line_count = 0;
+
+
+                while ((lineline = sr_txt.ReadLine()) != null)
+                {
+                    //Console.WriteLine(line_count + "\t" + lineline);
+                    template_content_262[line_count] = lineline;
+                    textBox1.Text += template_content_262[line_count];
+                    textBox1.Text += "\r\n";
+                    Console.WriteLine(line_count + "\t" + lineline);
+                    line_count++;
+                }
+            }
+            //Console.WriteLine(line_count);
+        }
+
+        private void button2_Click(object sender, EventArgs e) //gen
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+            #region get SN via barzode
+            SN_str = textBox_SN_1.Text;
+            byte[] bytes = Encoding.ASCII.GetBytes(SN_str); // string to dec
+            
+            String[] SN_split_16 = new String[16];
+            for (int i = 0; i < 16; i++)
+            {
+                SN_split_16[i] = bytes[i].ToString("X");
+                //Console.WriteLine((SN_split_16[i])+"\t");
+            }
+            #endregion
+
+            #region modify txt
+            for (int i = 0; i < 262; i++)
+            {
+                textBox3.Text += template_content_262[i];
+                textBox3.Text += "\r\n";
+
+                if (i == 50 || i == 52 || i == 54 || i == 56 ||
+                    i == 58 || i == 60 || i == 62 || i == 64 ||
+                    i == 66 || i == 68 || i == 70 || i == 72 ||
+                    i == 74 || i == 76 || i == 78 || i == 80)
+                {
+                    String addr = "";
+                    String data_str = "";
+
+
+                    switch (i)
+                    {
+                        //year
+                        case 50:
+                            addr = "30"; data_str = SN_split_16[0];
+                            break;
+
+                        //week
+                        case 52:
+                            addr = "32"; data_str = SN_split_16[1];
+                            break;
+                        case 54:
+                            addr = "34"; data_str = SN_split_16[2];
+                            break;
+
+                        //foxlink
+                        case 56:
+                            addr = "36"; data_str = SN_split_16[3];
+                            break;
+                        //Arena Model Number
+                        case 58:
+                            addr = "38"; data_str = SN_split_16[4];
+                            break;
+                        case 60:
+                            addr = "3A"; data_str = SN_split_16[5];
+                            break;
+                        case 62:
+                            addr = "3C"; data_str = SN_split_16[6];
+                            break;
+                        case 64:
+                            addr = "3E"; data_str = SN_split_16[7];
+                            break;
+
+                        //Arena
+                        case 66:
+                            addr = "40"; data_str = SN_split_16[8];
+                            break;
+                        case 68:
+                            addr = "42"; data_str = SN_split_16[9];
+                            break;
+                        //flag
+
+                        case 70:
+                            addr = "44"; data_str = SN_split_16[10];
+                            break;
+
+                        //serial number
+                        case 72:    //萬
+                            addr = "46"; data_str = SN_split_16[11];
+                            break;
+                        case 74:    //千
+                            addr = "48"; data_str = SN_split_16[12];
+                            break;
+                        case 76:    //百
+                            addr = "4A"; data_str = SN_split_16[13];
+                            break;
+                        case 78:    //十
+                            addr = "4C"; data_str = SN_split_16[14];
+                            break;
+                        case 80:    //個 
+                            addr = "4E"; data_str = SN_split_16[15];
+                            break;
+                    }
+                    textBox2.Text += addr + "\t";
+                    textBox2.Text += data_str;
+                    textBox2.Text += "\t00000000";
+                    textBox2.Text += "\r\n";
+                }
+                else
+                {
+                    textBox2.Text += template_content_262[i];
+                    textBox2.Text += "\r\n";
+                }
+            }
+            #endregion
+            System.IO.File.WriteAllText(folder_path + @"\" + SN_str + ".txt", textBox2.Text);
+        }
+    }
+}
